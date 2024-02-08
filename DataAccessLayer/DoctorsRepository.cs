a4717aed3e2e4313ce2c4b7992599817c0c12f3d
Sankar <sabarisankar25@gmail.com>
2024-02-08 10:27:15 +0530

    Add project files.

---
+++ DataAccessLayer/DoctorsRepository.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace DataAccessLayer
+{
+    public class DoctorsRepository: IDoctors
+    {
+        private readonly DbConnect connect;
+        public DoctorsRepository(DbConnect dbset)
+        {
+            connect=dbset;
+        }
+       public void Insert(Doctor details)
+        {
+            try
+            {
+                connect.Database.ExecuteSqlRaw("");
+            }
+            catch(Exception)
+            {
+
+            }
+        }
+        public void Update(Doctor replace,long id)
+        {
+            try
+            {
+                connect.Database.ExecuteSqlRaw("");
+            }
+            catch (Exception)
+            {
+
+            }
+
+        }
+        public void Delete(long id)
+        {
+            try
+            {
+                connect.Database.ExecuteSqlRaw("");
+            }
+            catch (Exception)
+            {
+
+            }
+        }
+         public List<Doctor> Getall()
+        {
+            try
+            {
+                var GetDetails = connect.DoctorTableList.FromSqlRaw<Doctor>("Select * from DoctorTableList").ToList();
+                return GetDetails;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+        public Doctor GetById(long id)
+        {
+            try
+            {
+                var GetidDetails = connect.DoctorTableList.FromSqlRaw<Doctor>("Select * from DoctorTableList").FirstOrDefault();
+                return GetidDetails;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+    }
+}
---
+++ DoctorsList/Controllers/DoctorController.cs
@@ -0,0 +1,117 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Configuration;
+using Microsoft.Extensions.Configuration;
+using DataAccessLayer;
+
+namespace DoctorsList.Controllers
+{
+    public class DoctorController : Controller
+    {
+        private readonly IDoctors obj;
+        private readonly string tableconnect;
+        public DoctorController(IDoctors Obj,IConfiguration service)
+        {
+            obj = Obj;
+            tableconnect = service.GetConnectionString("DbConnection");
+        }
+        // GET: DoctorController
+        public ActionResult Index()
+        {
+            return View("List");
+        }
+
+        // GET: DoctorController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: DoctorController/Create
+        public ActionResult Create(long?id)
+        {
+            if (id.HasValue)
+            {
+
+                var Doctors = obj.GetById(id.Value);
+
+                return View("Update", Doctors);
+            }
+            else
+            {
+                return View("Create", new Doctor());
+            }
+        }
+
+        // POST: DoctorController/Create
+        [HttpPost]
+       // [ValidateAntiForgeryToken]
+        public ActionResult Creates(Doctor details)
+        {
+            try
+            {
+                if (details.DoctorID == 0)
+                {
+                    obj.Insert(details);
+                }
+                else
+                {
+                    obj.Update(details, details.DoctorID);
+                }
+
+                return RedirectToAction(nameof(Index));
+               
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: DoctorController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: DoctorController/Edit/5
+        [HttpPost]
+        //[ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: DoctorController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: DoctorController/Delete/5
+        [HttpPost]
+        //[ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
